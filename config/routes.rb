c90a693afc9732ee8149d7efbd9b8f1f3486cb98
Coolsea <wei_918@coolsea.net>
2014-10-01 12:54:43 +0000

    新彈跳窗

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   root :to => "jobs#index"
   
   devise_for :users
-  resources :jobs
   
-
+  resources :jobs do 
+    collection do 
+      get :inform
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
