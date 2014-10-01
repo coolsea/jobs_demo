148764286104d11ae859076cfbcd75af8f91e4a4
Coolsea <wei_918@coolsea.net>
2014-10-01 04:43:05 +0000

    引入bootstrap & devise處理使用者權限

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  
+  
+  root :to => "jobs#index"
+  
+  devise_for :users
   resources :jobs
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
